6293bb41c7d5d45b6f0ad46819bc8174e4aa59e7
Fabricio Murillo Flores <fabriciomf72@gmail.com>
2025-03-23 17:58:49 -0600

    update rate limit

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hours window
-    max: 5, // limit each IP to 3 requests per windowMs
+    max: 4, // limit each IP to 4 requests per windowMs
     message: { error: 'You have exceeded the maximum number of attempts. Please try again later.' },
     standardHeaders: true,
     legacyHeaders: false,
